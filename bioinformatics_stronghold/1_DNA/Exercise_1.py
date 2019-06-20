763d5eddfb2c3ea723ec07e888c247f122862aed
JonasFritze <jonas.fritze@med.lu.se>
2019-06-20 16:28:21 +0200

    Init

---
+++ bioinformatics_stronghold/1_DNA/Exercise_1.py
@@ -0,0 +1,11 @@
+myfile = open("rosalind_dna.txt")
+dna_string = myfile.read().strip("\n")
+myfile.close()
+
+count = { "A":0, "C":0, "G":0, "T":0 }
+for i in count:
+    for j in dna_string:
+        if j == i:
+            count[i] += 1
+            
+print(" ".join(list(map(str, count.values()))))
